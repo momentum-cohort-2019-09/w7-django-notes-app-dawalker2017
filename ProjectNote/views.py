cc620826ea6ca41645272b5c0c3370ff18dd5710
danielwalker <dawalker2017@outlook.com>
2019-10-23 16:25:54 -0400

    10.23.19 04:24PM CHECKPOINT: Added CSS, Admin log-in, ability to add new entries through Admin Interface.

--- ProjectNote/views.py
+++ ProjectNote/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
+from ProjectNote.models import Note
 
 
 def notes_list(request):
-    return render(reqeust, "ProjectNote/templates/notes_list.html", {
-        "list": NOTES,
+    all_notes = Note.objects.all()
+    return render(request, "ProjectNote/notes_list.html", {
+        "notes": all_notes,
     })
 
 
-def notes_detail(request, id):
-    note_id = NOTES[id]
-    return render(request. "ProjectNote/templated/notes_list.html", {"note_id": note_id})
+# def notes_detail(request, id):
+#     note = Note.objects.get(id=id)
+#     return render(request, "ProjectNote/notes_detail.html", {"note": note})
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from ProjectNote import views
 
 urlpatterns = [
     path('', views.notes_list, name="notes_list"),
+    # path('note/', views.notes_detail, name="notes_detail"),
     path('admin/', admin.site.urls),
 ]
