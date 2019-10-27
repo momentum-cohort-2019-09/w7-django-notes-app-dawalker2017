6d0f6d5c32ab7b162369f29a3f7d30d5e9480136
danielwalker <dawalker2017@outlook.com>
2019-10-27 13:42:12 -0400

    10.27.19 1:41PM Checkpoint: Added Count and Date fields to Admin Interface.

--- ProjectNote/admin.py
+++ ProjectNote/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from ProjectNote.models import Note, NoteItem
 
-admin.site.register(Note)
+
+class NoteItemInline(admin.StackedInline):
+    model = NoteItem
+    extra = 0
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'item_count',
+        'created_at',
+        'updated_at'
+    )
+
+    inlines = [NoteItemInline]
+
+
+admin.site.register(Note, NoteAdmin)
 admin.site.register(NoteItem)
--- ProjectNote/models.py
+++ ProjectNote/models.py
@@ -2,16 +2,38 @@ from django.db import models
 
 
 class Note(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    title = models.CharField(
+        verbose_name="Note Title:",
+        max_length=100,
+        help_text="Max Characters: 100")
+    description = models.TextField(
+        verbose_name="Note Description:",
+        max_length=255,
+        help_text="Max Characters: 255",
+        blank=True,
+        null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def item_count(self):
+        return self.items.count()
 
     def __str__(self):
         return self.title
 
 
 class NoteItem(models.Model):
-    body = models.CharField(max_length=255)
+    body = models.CharField(
+        max_length=255,
+        help_text="Max Characters: 255",
+    )
     checklist = models.ForeignKey(
         to=Note, on_delete=models.CASCADE, related_name='items')
+
+    # order = models.PositiveIntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body
